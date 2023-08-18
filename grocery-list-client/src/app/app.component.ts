ca5af4b86d223a57fab721b53504120359d54861
Johnny Gérard <john@jgerard.dev>
2023-08-18 12:16:22 +0200

    test: check one Material component

--- grocery-list-client/src/app/app.component.ts
+++ grocery-list-client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
