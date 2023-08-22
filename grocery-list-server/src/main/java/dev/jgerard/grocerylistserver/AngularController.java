9a187508e499ea3d46043183775a3682610deace
Johnny Gérard <john@jgerard.dev>
2023-08-22 19:04:30 +0200

    feat: add Angular controller

---
+++ grocery-list-server/src/main/java/dev/jgerard/grocerylistserver/AngularController.java
@@ -0,0 +1,16 @@
+package dev.jgerard.grocerylistserver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AngularController {
+    /**
+     * Serve index.html to all paths without a file extension.
+     * This handler uses a legacy Ant-style path pattern.
+     */
+    @GetMapping("**/{path:[^.]*}")
+    public String serveAngularApp() {
+        return "forward:/";
+    }
+}
