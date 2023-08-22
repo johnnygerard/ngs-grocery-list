307e234cd42a6579cb27dcaad72ddf9d2b478906
Johnny Gérard <john@jgerard.dev>
2023-08-22 14:26:04 +0200

    feat: use snackBars for global error handling (#17)

--- grocery-list-client/src/app/error.interceptor.ts
+++ grocery-list-client/src/app/error.interceptor.ts
@@ -8,15 +8,13 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
       delay(error: HttpErrorResponse, retryCount: number) {
         // Retry non-HTTP errors after 1ms, 10ms, 100ms
         if (error.status === 0) {
-          console.error(`Retrying non-HTTP error (Attempt #${retryCount}):`, error);
+          console.error(`Retry failed request (Attempt #${retryCount})...`, error);
           return of(true).pipe(delay(10 ** (retryCount - 1)));
         }
 
-        if (error.status >= 400) {
-          console.error(error);
-          // Propagate user-friendly error message
+        // Propagate user-friendly error message
+        if (error.status >= 400)
           return throwError(() => error.error ?? Error('Unknown error'));
-        }
 
         throw Error('Invalid HTTP status code'); // Should never happen
       }
--- grocery-list-client/src/app/global-error-handler.ts
+++ grocery-list-client/src/app/global-error-handler.ts
@@ -1,7 +1,34 @@
-import { ErrorHandler } from "@angular/core";
+import { ErrorHandler, Injectable, NgZone } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class GlobalErrorHandler implements ErrorHandler {
-  handleError(error: Error): void {
-    window.alert('An error occurred: \n\n' + error.message);
+  constructor(
+    private _snackBar: MatSnackBar,
+    private _zone: NgZone,
+  ) { }
+
+  handleError(error: unknown): void {
+    let errorMessage = 'An unknown error occurred.';
+
+    console.error(error);
+
+    if (error) {
+      if (typeof error === 'object' && 'message' in error && error.message)
+        error = error.message;
+
+      if (typeof error === 'string' && error !== 'No message available')
+        errorMessage = error;
+    }
+
+    this._zone.run(() => {
+      this._snackBar.open(errorMessage, 'Dismiss', {
+        duration: 10_000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+      });
+    });
   }
 }
