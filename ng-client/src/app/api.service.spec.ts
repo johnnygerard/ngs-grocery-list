d2c23ea537aea638493b8baad2d7b9a8a60fb26c
Johnny Gérard <john@jgerard.dev>
2023-09-27 22:17:03 +0200

    test: add ApiService test for constructor

--- ng-client/src/app/api.service.spec.ts
+++ ng-client/src/app/api.service.spec.ts
@@ -1,18 +1,26 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ApiService } from './api.service';
+import { HttpClient } from '@angular/common/http';
+import { Product } from 'src/models/product.type';
+import { ApiService, BASE_URL } from './api.service';
+import { of } from 'rxjs';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpClientMock: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
-    });
-    service = TestBed.inject(ApiService);
+    httpClientMock = jasmine.createSpyObj('HttpClient', ['get']);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should instantiate and call getAllProducts', () => {
+    const products: Product[] = [
+      { id: 1n, name: 'APPLES', quantity: 2 },
+      { id: 2n, name: 'BANANAS', quantity: 3 },
+    ];
+    httpClientMock.get.withArgs(BASE_URL).and.returnValue(of(products));
+    service = new ApiService(httpClientMock);
+
+    expect(service.groceryList).toBe(products);
+    expect(httpClientMock.get).toHaveBeenCalledWith(BASE_URL);
+    expect(httpClientMock.get).toHaveBeenCalledTimes(1);
   });
 });
