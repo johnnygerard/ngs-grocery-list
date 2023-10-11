bb1c686c05bb091ce895be912dee2cb40c618e26
Johnny Gérard <john@jgerard.dev>
2023-10-11 16:31:01 +0200

    feat: add User entity class

---
+++ src/main/java/dev/jgerard/ngsgrocerylist/User.java
@@ -0,0 +1,41 @@
+package dev.jgerard.ngsgrocerylist;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
+    private String password;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
