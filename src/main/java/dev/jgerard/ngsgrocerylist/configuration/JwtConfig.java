414c0ad392d744a2d6e8934fb95894e00498a55b
Johnny Gérard <john@jgerard.dev>
2023-10-13 23:07:17 +0200

    feat: add generateJwt method + jwsAlgorithm

--- src/main/java/dev/jgerard/ngsgrocerylist/configuration/JwtConfig.java
+++ src/main/java/dev/jgerard/ngsgrocerylist/configuration/JwtConfig.java
@@ -3,19 +3,19 @@ package dev.jgerard.ngsgrocerylist.configuration;
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.*;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
 
 @Configuration
 public class JwtConfig {
     // Symmetric key for signing and verifying JWTs
     private final SecretKey secretKey;
+    private final MacAlgorithm jwsAlgorithm = MacAlgorithm.HS256;
 
     public JwtConfig() {
         final String ALGORITHM = "HmacSHA256";
@@ -35,11 +35,28 @@ public class JwtConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+        return NimbusJwtDecoder
+            .withSecretKey(secretKey)
+            .macAlgorithm(jwsAlgorithm)
+            .build();
     }
 
     @Bean
     public JwtEncoder jwtEncoder() {
         return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey));
+    }
+
+    public String generateJwt(String username) {
+        var now = Instant.now();
+        var header = JwsHeader.with(jwsAlgorithm).build();
+        var claims = JwtClaimsSet.builder()
+            .issuer("self")
+            .issuedAt(now)
+            .expiresAt(now.plusSeconds(3600))
+            .subject(username)
+            .build();
+        var parameters = JwtEncoderParameters.from(header, claims);
+
+        return jwtEncoder().encode(parameters).getTokenValue();
     }
 }
