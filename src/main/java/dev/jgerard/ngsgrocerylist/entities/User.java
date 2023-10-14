aa110516e0a72d95769310a2415167e1c1777836
Johnny Gérard <john@jgerard.dev>
2023-10-14 16:38:28 +0200

    fix: SQL syntax error with default "user" table name

--- src/main/java/dev/jgerard/ngsgrocerylist/entities/User.java
+++ src/main/java/dev/jgerard/ngsgrocerylist/entities/User.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Pattern;
 
 @Entity
+@Table(name = "end_user")
 public class User {
     @Id
     @GeneratedValue
