5d1f557e92050a6f5d36502f3e251a93b444ba20
Johnny Gérard <john@jgerard.dev>
2023-10-14 16:39:01 +0200

    style: reorder accessors

--- src/main/java/dev/jgerard/ngsgrocerylist/entities/User.java
+++ src/main/java/dev/jgerard/ngsgrocerylist/entities/User.java
@@ -19,12 +19,12 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getUsername() {
