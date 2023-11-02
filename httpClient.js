d7309619427f311aaab6414f97710a8a86943431
Johnny Gérard <john@jgerard.dev>
2023-11-02 08:38:06 +0100

    refactor: assert functions

--- httpClient.js
+++ httpClient.js
@@ -1,4 +1,22 @@
+// This file is imported by the HTTP Client.
 export const assertEqual = (expected, actual, message) => client.assert(
     expected === actual,
-    message + ` (expected: ${expected}, actual: ${actual})`
+    `${message} (expected: ${expected}, actual: ${actual})`
+);
+
+export const assertStatus = statusCode => assertEqual(
+    statusCode,
+    response.status,
+    'Invalid status code'
+);
+
+export const assertEmptyBody = () => client.assert(
+    '0' === response.headers.valueOf('Content-Length'),
+    'Non-empty body'
+);
+
+export const assertContentType = contentType => assertEqual(
+    contentType,
+    response.contentType.mimeType,
+    'Invalid content type'
 );
