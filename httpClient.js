892e3bb79812366bbab42c6985bf19da9489ff9a
Johnny Gérard <john@jgerard.dev>
2023-11-02 18:36:45 +0100

    feat: add assertEqualJson

--- httpClient.js
+++ httpClient.js
@@ -21,3 +21,32 @@ export const assertContentType = contentType => assertEqual(
     response.contentType.mimeType,
     'Invalid content type'
 );
+
+export const assertEqualJson = (expected, actual) => {
+    assertContentType('application/json');
+
+    client.assert(
+        isDeepEqual(expected, actual),
+        `JSON values are not equal:
+Expected: ${expected}
+Actual: ${actual}`
+    );
+};
+
+const isDeepEqual = (expected, actual) => {
+    if (expected === actual) return true;
+    if (typeof expected !== 'object' || typeof actual !== 'object')
+        return false;
+
+    const expectedKeys = Object.keys(expected);
+    const actualKeys = Object.keys(actual);
+
+    if (expectedKeys.length !== actualKeys.length) return false;
+
+    for (let key of expectedKeys) {
+        if (!actualKeys.includes(key)) return false;
+        if (!isDeepEqual(expected[key], actual[key])) return false;
+    }
+
+    return true;
+};
