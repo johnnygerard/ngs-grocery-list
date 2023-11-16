6b117d0d113045306cef985679c425aee9d4d7ba
Johnny Gérard <john@jgerard.dev>
2023-11-16 22:41:21 +0100

    test: add addProducts test method

--- src/it/java/dev/jgerard/ngsgrocerylist/it/ApiIT.java
+++ src/it/java/dev/jgerard/ngsgrocerylist/it/ApiIT.java
@@ -1,13 +1,21 @@
 package dev.jgerard.ngsgrocerylist.it;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.jgerard.ngsgrocerylist.ProductName;
+import dev.jgerard.ngsgrocerylist.entities.Product;
+import org.json.JSONException;
 import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -15,22 +23,57 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @TestClassOrder(ClassOrderer.OrderAnnotation.class)
 class ApiIT {
-    private static final String CREDENTIALS_1 = "Alice,pdH8W8zL";
-    private static final String CREDENTIALS_2 = "Bob,P5uQ9iHH";
+    private static final String BASE_URL = "/api/products";
+    // Most tests will target the main user.
+    private static final String MAIN_USER = "Alice";
+    // The main reason for having a secondary user is to
+    // ensure grocery lists are user-specific.
+    private static final String SECONDARY_USER = "Bob";
+    private static final String CREDENTIALS_1 = MAIN_USER + ",pdH8W8zL";
+    private static final String CREDENTIALS_2 = SECONDARY_USER + ",P5uQ9iHH";
     private static final String CREDENTIALS_TEMPLATE = "username=%s&password=%s";
-    private final List<String> jwts = new ArrayList<>();
+    private static final List<String> jwts = new ArrayList<>();
+    private static final List<Product> mainGroceryList;
+    private static final List<Product> secondaryGroceryList;
+
+    static {
+        mainGroceryList = List.of(
+            makeProduct(ProductName.APPLES, 3),
+            makeProduct(ProductName.BANANAS, 2),
+            makeProduct(ProductName.CARROTS, 1)
+        );
+
+        secondaryGroceryList = List.of(
+            makeProduct(ProductName.BREAD, 1),
+            makeProduct(ProductName.MILK, 10)
+        );
+    }
+
     @Autowired
     private TestRestTemplate restTemplate;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private static Product makeProduct(ProductName name, int quantity) {
+        var product = new Product();
+        product.setName(name);
+        product.setQuantity(quantity);
+        return product;
+    }
 
     @Nested
     @Order(1)
@@ -54,6 +97,46 @@ class ApiIT {
             );
 
             jwts.add(response.getBody());
+        }
+
+        @Test
+        @Order(2)
+        void addProducts() throws JsonProcessingException, JSONException {
+            var locationPattern = Pattern.compile(BASE_URL + "/(\\d+)");
+            var groceryLists = List.of(mainGroceryList, secondaryGroceryList);
+
+            for (int i = 0; i < groceryLists.size(); i++) {
+                List<Product> groceryList = groceryLists.get(i);
+                String jwt = jwts.get(i);
+
+                for (Product product : groceryList) {
+                    var request = RequestEntity.post(BASE_URL)
+                        .header("Authorization", "Bearer " + jwt)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(product);
+                    var response = restTemplate.exchange(request, Void.class);
+                    URI location = response.getHeaders().getLocation();
+
+                    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+                    assertNotNull(location, "Location header not found");
+                    var matcher = locationPattern.matcher(location.toString());
+                    assertTrue(matcher.matches());
+                    product.setId(Long.valueOf(matcher.group(1)));
+                    assertNull(response.getBody());
+                }
+
+                var request = RequestEntity.get(BASE_URL)
+                    .header("Authorization", "Bearer " + jwt)
+                    .build();
+                var response = restTemplate.exchange(request, String.class);
+                assertEquals(HttpStatus.OK, response.getStatusCode());
+                assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+                JSONAssert.assertEquals(
+                    objectMapper.writeValueAsString(groceryList),
+                    response.getBody(),
+                    JSONCompareMode.NON_EXTENSIBLE
+                );
+            }
         }
     }
 
